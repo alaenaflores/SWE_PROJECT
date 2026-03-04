9ae0bd116e7be8000a8eaad44cbff7629f98a2ae
Monica Coira <monica.coira@ufl.edu>
2026-03-04 16:56:37 -0500

    implement user authentication routes

--- server/index.js
+++ server/index.js
@@ -3,10 +3,28 @@ const mongoose = require('mongoose');
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+const session = require('express-session');
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+}));
+
+app.use(session({
+    secret: "nutriventure-key",   
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false,   
+        maxAge: 1000 * 60 * 60 * 60 
+    }
+}));
+
+const authRoutes = require('./routes/auth.js');
+app.use('/auth', authRoutes);
 
 mongoose.connect(process.env.DB_URI)
     .then(() => {
