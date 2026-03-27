d6840422f2cf6aaf6c159054881487631491432c
Monica Coira <monica.coira@ufl.edu>
2026-03-27 19:36:22 -0400

    implement streak calculation backend

--- server/routes/meals.js
+++ server/routes/meals.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const Meal = require("../models/Meal");
+const User = require("../models/User");
 
 const router = express.Router();
+const  updateStreak = require("../utils/updateStreak");
 
 const requireAuth = (req, res, next) => {
     if (!req.session.userId) {
@@ -13,6 +15,7 @@ const requireAuth = (req, res, next) => {
 router.post("/", requireAuth, async (req, res) => {
     try {
         const { date, mealType, items } = req.body;
+        const user = await User.findById(req.session.userId);
 
         if (!date || !mealType) {
             return res.status(400).json({ error: "Date and meal type are required" });
@@ -25,7 +28,10 @@ router.post("/", requireAuth, async (req, res) => {
             items: items || []
         });
 
-        res.status(201).json(meal);
+        updateStreak(user);
+        await user.save();
+
+        res.status(201).json({meal, currentStreak: user.currentStreak});
     } catch (error) {
         res.status(500).json({ error: "Could not create meal" });
     }
---
+++ server/utils/updateStreak.js
@@ -0,0 +1,28 @@
+const updateStreak = (user) => {
+    const today = new Date();
+    const lastLogged = user.lastLoggedDate;
+
+    const todayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const lastDate = lastLogged
+        ? new Date(lastLogged.getFullYear(), lastLogged.getMonth(), lastLogged.getDate())
+        : null;
+    const timeDifference = lastDate ? todayDate - lastDate : null;
+    const dayDifference = timeDifference ? timeDifference / (1000 * 60 * 60 * 24) : null;
+
+    if (dayDifference === 0){
+        return;
+    }
+
+    if (!lastDate) {
+        user.currentStreak = 1;
+    } else if (dayDifference === 1) {
+        user.currentStreak += 1;
+    } else if (dayDifference > 1) {
+        user.currentStreak = 1;
+    }
+
+    if (user.currentStreak > user.longestStreak) {
+        user.longestStreak = user.currentStreak;
+    }
+    user.lastLoggedDate = todayDate;
+};
